c3de50ec1d09126cc766471b0a8903e8d9571b43
aghp81 <aghp81@gmail.com>
2024-08-07 10:04:29 +0330

    دسترسی به کتابخانه Cheerio -  مشخص کردن محتوای html

---
+++ index.js
@@ -0,0 +1,9 @@
+// دسترسی به کتابخانه Cheerio
+const cheerio  = require("cheerio");
+
+// مشخص کردن محتوای html
+const $ = cheerio.load(`
+    <html><head><title>
+        Hello, World!
+    </title></head></html>
+`)

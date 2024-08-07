3b84da8680631c537b50ac5718619b4e6f73bcb7
aghp81 <aghp81@gmail.com>
2024-08-07 10:15:36 +0330

    همه محتوای html را لود می کند

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const $ = cheerio.load(`
     <html><head><title>
         Hello, World!
     </title></head></html>
+    <p>sample text</p>
 `)
 
 console.log($('title').text()); // node index.js == Hello, World!
 console.log($('head').text()); // node index.js == Hello, World!
 console.log($('head').html()); // node index.js == <html code>
+console.log($.html()); // همه محتوای html را لود می کند

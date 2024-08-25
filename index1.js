acf82810c7861d6e0744e56f37a7e8d2392357bc
aghp81 <aghp81@gmail.com>
2024-08-25 19:46:22 +0330

    await axios.get(url)

--- index1.js
+++ index1.js
@@ -1,4 +1,12 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const fetchCategorise = async () => {};
+const fetchCategorise = async () => {
+  try {
+    const url = "https://faradars.org";
+
+    const response = await axios.get(url);
+  } catch (error) {
+    console.log("Error Fetching Categorise: ", error);
+  }
+};

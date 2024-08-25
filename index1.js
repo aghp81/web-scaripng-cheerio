e1a5f26444f384d078a733ad280f822c5393244c
aghp81 <aghp81@gmail.com>
2024-08-25 20:00:01 +0330

    headers - کاربر از چه مرورگری استفاده می کند

--- index1.js
+++ index1.js
@@ -5,8 +5,13 @@ const fetchCategorise = async () => {
   try {
     const url = "https://faradars.org";
 
-    const response = await axios.get(url,{
-        timeout: 10000, // 10000ms wait for response the data from site
+    const response = await axios.get(url, {
+      timeout: 10000, // 10000ms wait for response the data from site
+      // به سرور اعلام می شود کلاینتی که درخواست را میفرستد از چه مرورگری استفاده می کند و یک کاربر واقعی است
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",
+      },
     });
 
     const $ = cheerio.load(response.data);
